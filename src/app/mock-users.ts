73c6166a59319043c870e064d0007100bc99e63c
2mttm <twometrue@gmail.com>
2024-05-03 14:38:21 +0300

    Format code

--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -1,61 +1,61 @@
 import {User} from "./user";
 
 export const users: User[] = [
-  {
-    userId: "09452W4295001",
-    userInitials: "EB",
-    fullName: "Elisa Blu"
-  },
-  {
-    userId: "09452W4295002",
-    userInitials: "SN",
-    fullName: "Sara Neri"
-  },
-  {
-    userId: "09452W4295003",
-    userInitials: "CD",
-    fullName: "Clone Dispo"
+    {
+        userId: "09452W4295001",
+        userInitials: "EB",
+        fullName: "Elisa Blu"
+    },
+    {
+        userId: "09452W4295002",
+        userInitials: "SN",
+        fullName: "Sara Neri"
+    },
+    {
+        userId: "09452W4295003",
+        userInitials: "CD",
+        fullName: "Clone Dispo"
 
-  },
-  {
-    userId: "09452W4295010",
-    userInitials: "MR",
-    fullName: "Mario Rossi"
-  },
+    },
+    {
+        userId: "09452W4295010",
+        userInitials: "MR",
+        fullName: "Mario Rossi"
+    },
 
-  {
-    userId: "09452W4295011",
-    userInitials: "PP",
-    fullName: "Pinco Pallino"
-  },
-  {
-    userId: "09452W4295012",
-    userInitials: "TB",
-    fullName: "Titi Boom"
-  },
+    {
+        userId: "09452W4295011",
+        userInitials: "PP",
+        fullName: "Pinco Pallino"
+    },
+    {
+        userId: "09452W4295012",
+        userInitials: "TB",
+        fullName: "Titi Boom"
+    },
 
-  {
-    userId: "39957125648321",
-    userInitials: "RC",
-    fullName: "Roberto Carlos"
-  },
+    {
+        userId: "39957125648321",
+        userInitials: "RC",
+        fullName: "Roberto Carlos"
+    },
 
-  {
-    userId: "0945231595011",
-    userInitials: "PE",
-    fullName: "Pablo Escobaro"
-  },
+    {
+        userId: "0945231595011",
+        userInitials: "PE",
+        fullName: "Pablo Escobaro"
+    },
 
-  {
-    userId: "3658415713594sda",
-    userInitials: "DT",
-    fullName: "Dominik Toretto"
-  },
+    {
+        userId: "3658415713594sda",
+        userInitials: "DT",
+        fullName: "Dominik Toretto"
+    },
 
-  {
-    userId: "4re9a35s411564+41",
-    userInitials: "PW",
-    fullName: "Paul Walker"
-  }
+    {
+        userId: "4re9a35s411564+41",
+        userInitials: "PW",
+        fullName: "Paul Walker"
+    }
 
 ];
